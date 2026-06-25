580f748209e443fdde7d659c7b1cc0ff40508c94
01BARRETO1 <karbar_1990@hotmail.com>
2026-06-25 16:09:12 -0500

    PARTE 2, del 12 al 19

--- cazando.js
+++ cazando.js
@@ -1,8 +1,31 @@
 let canvas= document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
+//Variables iniciadas en cero
+let gatoX=canvas.width/2;
+let gatoY=canvas.height/2;
+let comidaX=0;
+let comidaY=0;
+//Constantes:
+const ALTO_GATO=30;
+const ANCHO_GATO=80;
+const ALTO_COMIDA=10;
+const ANCHO_COMIDA=10;
+
 function graficarGato(){
     ctx.fillStyle="#ce4a07";
-    ctx.fillRect(canvas.width/2-40,canvas.height/2-30,80,30);
+    ctx.fillRect(gatoX-ANCHO_GATO/2,gatoY-ALTO_GATO,ANCHO_GATO,ALTO_GATO);
+}
 
+function graficarComida(){
+    ctx.fillStyle="#0fa904";
+    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA);
+}
+
+function iniciarJuego(){
+    graficarGato();
+    ctx.fillStyle="#0fa904";
+    comidaX=490;
+    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA);
+    graficarComida();
 }
